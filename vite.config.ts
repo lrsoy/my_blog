5db4d2d04c3aca8350b58dd21e5af906bd704417
lrsoy <lying_jk_bp@163.com>
2022-07-14 14:58:53 +0800

    安装自动引入API，自动引入组件插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      //为true时在项目根目录自动创建
+      dts: 'types/auto-imports.d.ts',
+    }),
+    Components({
+      dirs: ['src/components'],
+      directoryAsNamespace: true,
+      dts: 'types/components.d.ts',
+    })
+  ]
 })
