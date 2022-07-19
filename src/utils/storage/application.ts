0a4a5347432ff8aed995206879a1a62d85b3dac8
YING <69709362+lrsoy@users.noreply.github.com>
2022-07-19 20:39:04 +0800

    增加工具

---
+++ src/utils/storage/application.ts
@@ -0,0 +1,31 @@
+/** 处理本地存储 
+ *  localStorage
+ *  sessionStorage
+*/
+interface data {
+  data: any,
+  export?: number
+}
+export const loStorage = {
+  set(key: string, data: any, expire?: number): void {
+
+  },
+  get() {
+
+  },
+  remove() {
+
+  }
+}
+
+export const seStorage = {
+  set() {
+
+  },
+  get() {
+
+  },
+  remove() {
+
+  }
+}
