4591a2c96f9bc7045c2d484bf329f5937477ddd0
lrsoy <lying_jk_bp@163.com>
2022-07-24 19:06:19 +0800

    修改布局

--- src/views/home/components/home-banner.vue
+++ src/views/home/components/home-banner.vue
@@ -6,17 +6,17 @@ import HomeNav from './home-nav.vue'
 <template>
   <header class="home-banner">
     <HomeNav />
-    <div class="enter-blog">
+    <!-- <div class="enter-blog">
       <span class="enter">
         <a href="#">Enter Blog</a>
       </span>
-    </div>
+    </div>-->
   </header>
 </template>
 
 <style lang="scss" scoped>
 .home-banner {
-  @apply relative w-full h-[600px] bg-[url('~/assets/images/banner.jpg')] bg-cover bg-center bg-no-repeat; // bg-fixed
+  @apply relative w-full h-[500px] bg-[url('~/assets/images/pexels-photo-220116.jpeg')] bg-cover bg-center bg-no-repeat; // bg-fixed
   &::after {
     @apply absolute -bottom-20 left-0  h-36 w-full bg-[url('~/assets/images/h1-rev-bottom.png')]  bg-cover bg-center bg-no-repeat;
     content: "";
--- src/views/home/components/home-main-list.vue
+++ src/views/home/components/home-main-list.vue
@@ -3,16 +3,38 @@
 <script setup lang="ts">
 </script>
 <template>
-  <section class="home-main-list">
-    <div class="continer">创建组件</div>
+  <section class="home-main-list-recommend">
+    <div class="continer">
+      <template v-for="item in 10">
+        <div class="item">
+          <div class="img"></div>
+          <div class="content">
+            <div>头像</div>
+            <div class="classification">标签分类</div>
+            <h2>标题文字</h2>
+            <div>
+              <p>段落内容</p>
+            </div>
+            <div class="operate">操作</div>
+          </div>
+        </div>
+      </template>
+    </div>
   </section>
 </template>
 
 <style lang="scss">
-.home-main-list {
+.home-main-list-recommend {
   @apply w-full h-auto flex justify-center;
   .continer {
-    @apply w-[1200px] h-auto bg-slate-700 mt-20;
+    @apply w-[1200px] h-auto  mt-20 columns-4 gap-[20px];
+    .item {
+      @apply w-full h-auto bg-slate-600;
+      break-inside: avoid;
+      .img {
+        @apply w-full h-auto;
+      }
+    }
   }
 }
 </style>
